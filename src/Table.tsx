21a64808aea8af451ce86a4a442325b5aa9bfe1f
jf <jpf0628@gmail.com>
2022-07-28 17:11:59 -0700

    Finished basic game start/restart button - card functionality - gameover msg

--- src/Table.tsx
+++ src/Table.tsx
@@ -3,7 +3,7 @@ import Card from './Card'
 import { CardInterface } from './interfaces';
 import { getDeckId, getCards, dupCards, shuffle } from './utils/deck';
 import './Table.css'
-import { v4 as uuidv4 } from 'uuid';
+import { v4 } from 'uuid';
 
 
 /** Table component renders n sets of cards (default 7 sets)
@@ -17,10 +17,13 @@ function Table() {
   const [cards, setCards] = useState([]);
   const [deckId, setDeckId] = useState<number>(0);
   const [isDisabled, setIsDisabled] = useState<boolean>(false);
-  const [flippedCards, setFlippedCards] = useState<any[]>([]);
+  const [flippedCards, setFlippedCards] = useState<Element[]>([]);
+  const [correctMatches, setCorrectMatches] = useState<number>(0);
+  const [isGameOver, setIsGameOver] = useState<boolean>(false);
 
   /** return deck id by calling on getDeckId() */
   async function fetchDeckId(): Promise<void> {
+    setCorrectMatches(0);
     const deckId = await getDeckId();
     setDeckId(deckId)
   }
@@ -50,7 +53,7 @@ function Table() {
 
 
   /** checks if cards match based id(card code) */
-  function isAMatch(card1: any, card2: any) {
+  function isAMatch(card1: Element, card2: Element) {
     return card1.id === card2.id;
   }
 
@@ -72,27 +75,27 @@ function Table() {
         flippedCards[1].classList.add('disable-pointer')
         setIsDisabled(false);
         setFlippedCards([]);
+        setCorrectMatches(matches => matches + 2)
       }
     }
+    if (correctMatches === cards.length) setIsGameOver(true);
   }, [flippedCards])
 
 
-  /** toggle disabled */
-  function toggleDisable(): void {
-    setIsDisabled(!isDisabled);
-  }
-
   return (
     <>
-      <p>Table</p>
-      <button onClick={fetchDeckId}>Click me to set table</button>
+      <h1>Memory Game</h1>
+      <button onClick={fetchDeckId}>{cards.length === 0 ? "play a game?" : "restart"}</button>
       <section className={isDisabled ? 'memory-game disable-pointer' : 'memory-game'}>
         {cards.map((card: CardInterface) =>
-          <section id={card.code} className='memory-card' onClick={(e:any)=>flipCard(e)}>
-            <Card key={uuidv4()} card={card} />
+          <section id={card.code} className='memory-card' onClick={(e: SyntheticEvent) => flipCard(e)}>
+            <Card key={v4()} card={card} />
           </section>
         )}
       </section>
+      <h3>
+        {isGameOver && correctMatches === cards.length ? "Congrats you did it!!!" : ""}
+      </h3>
     </>
   )
 }
